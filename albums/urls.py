702c3ead67e893a9a1eeb8c6f7f3574cf397f61b
Nathan Zeager <nathanzeager@hotmail.com>
2022-08-22 21:50:10 -0400

    Generic home page displays

---
+++ albums/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.album_list, name='album_list')
+]
--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def album_list(request):
+    return render(request, 'albums/album_list.html', {})
