84b4d0c17c442738031a38e288ec7c456998a6df
Nathan Zeager <nathanzeager@hotmail.com>
2022-08-23 11:35:27 -0400

    Home page shows albums

--- albums/models.py
+++ albums/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 
 class Album(models.Model):
-    titles = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} by {self.artist}'
--- albums/views.py
+++ albums/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Album
 
 
 def album_list(request):
-    return render(request, 'albums/album_list.html', {})
+    # In the line below we could have filtered which albums to get, but we got all of them
+    albums = Album.objects.all()
+    return render(request, 'albums/album_list.html', {'albums': albums})
