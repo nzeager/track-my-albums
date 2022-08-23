ccf1d36042bc1163bb97d083d3806eb3897555b4
Nathan Zeager <nathanzeager@hotmail.com>
2022-08-23 15:39:11 -0400

    Added links to details page

--- albums/urls.py
+++ albums/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.album_list, name='album_list')
+    path('', views.album_list, name='album_list'),
+    path('albums/<int:pk>', views.album_detail, name='album_detail')
+    # path('albums/new', views.create_album, name='create_album')
 ]
--- albums/views.py
+++ albums/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 
@@ -6,3 +6,13 @@ def album_list(request):
     # In the line below we could have filtered which albums to get, but we got all of them
     albums = Album.objects.all()
     return render(request, 'albums/album_list.html', {'albums': albums})
+
+
+def album_detail(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    return render(request, 'albums/album_detail.html', {"album": album})
+
+
+# Finish
+# def create_album(request):
+#     return render(request, 'albums/create_album.html',{'form': form})
