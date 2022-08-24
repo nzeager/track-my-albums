12491d1b5b69dc09e80fd1b9343b209259a73891
Nathan Zeager <nathanzeager@hotmail.com>
2022-08-24 11:24:00 -0400

    Added edit functionality

--- albums/views.py
+++ albums/views.py
@@ -15,14 +15,26 @@ def album_detail(request, pk):
     return render(request, 'albums/album_detail.html', {"album": album})
 
 
-# Finish
 def create_album(request):
     if request.method == "POST":
         form = AlbumForm(request.POST)
         if form.is_valid():
             album = form.save()
             album.save()
-            return redirect('album_list')
+            return redirect('album_detail', pk=album.pk)
     else:
         form = AlbumForm()
+    return render(request, 'albums/create_album.html', {'form': form})
+
+
+def album_edit(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    if request.method == "POST":
+        form = AlbumForm(request.POST, instance=album)
+        if form.is_valid():
+            album = form.save(commit=False)
+            album.save()
+            return redirect('album_detail', pk=album.pk)
+    else:
+        form = AlbumForm(instance=album)
     return render(request, 'albums/create_album.html', {'form': form})
